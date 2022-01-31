3923350af69e788e31f7a937bd76ae1877ed659a
MikeLekutit <mikesalonik2@gmail.com>
2022-01-31 13:54:09 +0300

    moday 31st

---
+++ divisibility.py
@@ -0,0 +1,14 @@
+number = int(input("Enter a number:"))
+if(number % 2 == 0):
+  print("even")
+else:
+  print("odd")
+
+
+
+
+  number = int(input("Enter a number:"))
+if(number % 5 == 0):
+  print("divisible")
+else:
+  print("not divisible")
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+james = int(input("enter height for james:"))
+mike = int(input("enter height for mike:"))
+jude = int(input("enter height for jude:"))
+if james > mike and jude:
+  print("james")
+if mike > james and jude:
+  print("mary")
+if jude > james and mike:
+  print("jude")
