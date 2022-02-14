dd946c601173f846574353f503a09fdcceaf808a
MikeLekutit <mikesalonik2@gmail.com>
2022-02-14 09:59:22 +0300

    14th feb

---
+++ sum_in_a_list.py
@@ -0,0 +1,12 @@
+total = 0
+list1 = [11, 5, 17, 18, 23]
+for ele in range(0, len(list1)):
+    total = total + list1[ele]
+print("Sum of all elements in given list: ", total)
+
+
+list =[1,2,3,7]
+sum = 0
+for x in list:
+	sum = sum + x
+	print("sum=",sum)
