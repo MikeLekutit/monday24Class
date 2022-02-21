be8d89fd0be29b4f362bf73232242551e38e7293
MikeLekutit <mikesalonik2@gmail.com>
2022-02-21 09:57:02 +0300

    21st feb

---
+++ string.py
@@ -0,0 +1,7 @@
+string1= "pythonprogramming"
+print("\nslicing characters from 4-10:")
+print(string1[4:10])
+
+string1 = "|{:<10}|{:^10}|{:>10}".format('geeks' , 'for' , 'geeks')
+print("\nleft, centre and right alignment with formatting:")
+print(string1)
